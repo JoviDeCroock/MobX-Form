ac85704310247729fc999ac1f5e51e64259e66c3
Jovi De Croock <jovi@codifly.be>
2018-02-11 20:54:51 +0100

    task: prepare for devving

--- src/Form.js
+++ src/Form.js
@@ -1,22 +1,3 @@
-reduxForm({
-  form: 'createSegment',
-  validate: (values) => {
-    const schema = Joi.object().keys({
-      criterionId: Joi.string(),
-      innoBoardId: Joi.string(),
-      title: Joi.string().max(255).required(),
-      description: Joi.string().optional(),
-    });
-    const { error } = Joi.validate(values, schema, { allowUnknown: true });
-    const versionErrors = translateJoiError(error);
-    const groupErrors = validateGroupFields(values);
-    const errors = { ...versionErrors, ...groupErrors };
-    return errors;
-  },
-});
-
-import { observable, action, computed } from 'mobx';
-
 export default class Form {
   name;
   // Function to validate from
