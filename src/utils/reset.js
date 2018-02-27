f6e9216858e631aed862159249de8b33b76242ca
Jovi De Croock <jovi@codifly.be>
2018-02-27 18:10:38 +0100

    feat: finetune the reset utils

--- src/utils/reset.js
+++ src/utils/reset.js
@@ -1,23 +1,15 @@
 function reset(value) {
   switch (typeof value) {
-    case 'string': {
-      return '';
-    }
-    case 'number': {
-      return 0;
-    }
-    case 'boolean': {
-      return false;
-    }
+    case 'string': return '';
+    case 'number': return 0;
+    case 'boolean': return false;
     case 'object': {
       if (value instanceof Date) {
         return new Date();
       }
       return {};
     }
-    default: {
-      return undefined;
-    }
+    default: return undefined;
   }
 }
 
